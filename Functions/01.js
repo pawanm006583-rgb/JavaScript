747bab154cd3c45f5929c312d81b3926aba1d76b
Pawan Mishra <pawanm006583@@gmail.com>
2026-01-20 20:25:03 +0530

    first commit

---
+++ Functions/01.js
@@ -0,0 +1,15 @@
+let choice = "tea";
+
+switch(choice){
+    case "tea":
+    console.log("tea price 10rs");
+    break;
+    case "coffee":
+    console.log("Coffee price: 20rs");
+    break;
+    case "juice":
+    console.log("Juice price: 40rs");
+    break;
+    default:
+    console.log("Iteam not available");
+}
