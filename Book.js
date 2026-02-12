a620feb8dc282aed5345f7f6b331e06211197f33
Pawan Mishra <pawanm006583@gmail.com>
2026-02-12 20:32:39 +0530

    Object

---
+++ Book.js
@@ -0,0 +1,23 @@
+// let book ={
+//     title: "The Aurelinus",
+//     name: "Meditations",
+//     price: 250,
+
+//     getDetails:function(){
+//         return "Book"
+//     }
+// }
+
+
+
+
+let person ={
+    name:"Alex",
+    age: 23,
+    city: "Nashik"
+};
+
+getDetails.functionperson(person.name()) {
+    return "Name"+
+}
+console.log(person.name());
