282ebd8c494922442937cffb035c5ac7cbaf94ba
Pawan Mishra <pawanm006583@gmail.com>
2026-02-12 20:33:20 +0530

    01.js

---
+++ 01.js
@@ -0,0 +1,8 @@
+let subject =["HTML","CSS","JAVASCRIPT","PYTHON","C++"];
+let marks =[20,30,40,50];
+let student =["Alex", "ROMANIO", "RAHUL"];
+
+console.log("subject",student);
+console.log("marks",marks);
+console.log("student",student);
+
